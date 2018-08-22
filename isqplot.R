0adc1268da97d126d08fd8c3d1909c5868ec68c8
Rothanak So <rothanak.hello@gmail.com>
2018-08-22 01:13:40 -0400

    Add "focused" scatterplot variant

--- isqplot.R
+++ isqplot.R
@@ -5,6 +5,7 @@ library(ggplot2)
 library(ggplot2bdc)
 library(ggalt)
 library(scales)
+library(plyr)
 library(dplyr)
 
 #' ### Data
@@ -13,38 +14,83 @@ library(dplyr)
 #' from any source so long as all the required columns are satisfied. Check out
 #' [ISQool](https://github.com/rothso/isqool) to learn how to generate CSVs for
 #' other UNF courses.
-course <- "COP2220"
-df <- read.csv(paste0("data/", course, ".csv")) %>%
-  select(course, term, instructor, rating, average_gpa) %>%
-  na.omit()
+fileName <- "COP2220"
+read_data <- function(fileName) {
+  read.csv(paste0("data/", fileName, ".csv")) %>%
+    select(course, term, instructor, rating, average_gpa) %>%
+    na.omit()
+}
+df <- read_data(fileName)
 
 #' `r knitr::kable(head(df), row.names = FALSE)`
 
 #' Based on the file name, we can determine if the file represents one course
 #' and various professors, like above, or one professor and various courses.
 #' We're plotting the *feature* that varies (as the other will be constant).
-if (startsWith(course, "N") && nchar(course) == 9) {
-  feature <- "Course"
-  df$feature <- df$course
-} else {
-  feature <- "Instructor"
-  df$feature <- df$instructor
+add_feature <- function(df, fileName) {
+  isProfessor <- startsWith(fileName, "N") && nchar(fileName) == 9
+  if(isProfessor) {
+    df$feature = df$course
+    comment(df$feature) <- "Course"
+  } else {
+    df$feature = df$instructor
+    comment(df$feature) <- "Instructor"
+  }
+  df
 }
+df <- add_feature(df, fileName)
 
 #' ### Scatter Plot
 #+ scatterplot
 ggplot(df, aes(x = rating, y = average_gpa, color = feature)) +
+geom_encircle(aes(fill = feature), s_shape = 0.7, expand = 0.02, spread = 0.015, alpha = 0.1) +
+geom_text(aes(label = feature), nudge_y = -0.035, size = 3) +
+geom_point() +
+guides(size = guide_legend(override.aes = list(linetype = 0))) +
+labs(
+  title = fileName,
+  subtitle = paste0("Course evaluation results from ", nrow(df), " classes"),
+  caption = "Source: UNF ISQ Departmental Data Summary",
+  color = comment(df$feature),
+  fill = comment(df$feature),
+  x = "Average Student Rating",
+  y = "Average GPA"
+) +
+theme_bdc_grey(grid.x = TRUE, grid.y = TRUE) +
+theme(legend.position = "right", legend.direction = "vertical", legend.title.align = 0)
+
+#' ### Focused Scatter Plot
+#+ echo=FALSE
+fileName <- "ENC1143"
+df <- read_data(fileName) %>%
+  add_feature(fileName)
+
+#' If a plot contains a lot of data (400+ points), it would be nice to
+#' differentiate only those points which we care about. We can highlight a
+#' specific professor we're interested in by reducing our feature to just two
+#' levels.
+highlight <- "Berry"
+df$feature <- ifelse(df$feature == highlight, highlight, "Other")
+
+#' Let's gray out the other data points and use blue as our accent color.
+palette <- c("#003886", "#CCD7E7")
+names(palette) <- c(highlight, "Other")
+
+#' We'll then adjust the `geom_text` to hide superfluous labels and move the
+#' `geom_points` behind the text.
+#+ focusplot
+ggplot(df, aes(rating, average_gpa, color = feature)) +
   geom_encircle(aes(fill = feature), s_shape = 0.7, expand = 0.02, spread = 0.015, alpha = 0.1) +
-  geom_text(aes(label = feature), nudge_y = -0.035, size = 3) +
   geom_point() +
-  scale_size_continuous(labels = percent, range = c(1, 2.5)) +
-  guides(size = guide_legend(override.aes = list(linetype = 0))) +
+  geom_text(aes(label = ifelse(feature == highlight, highlight, "")), nudge_y = -0.035, size = 3) +
+  scale_color_manual(values = palette) +
+  scale_fill_manual(values = palette) +
   labs(
-    title = paste0(course),
+    title = fileName,
     subtitle = paste0("Course evaluation results from ", nrow(df), " classes"),
     caption = "Source: UNF ISQ Departmental Data Summary",
-    color = feature,
-    fill = feature,
+    color = comment(df$feature),
+    fill = comment(df$feature),
     x = "Average Student Rating",
     y = "Average GPA"
   ) +
@@ -57,4 +103,4 @@ ggplot(df, aes(x = rating, y = average_gpa, color = feature)) +
 last_plot() + theme(plot.margin = margin(2, 2, 2, 2, "cm"))
 
 # Save as a 15 x 8 inch image
-ggsave(paste0(course, ".png"), width = 14, height = 8, dpi = 100)
+ggsave(paste0(fileName, ".png"), width = 14, height = 8, dpi = 100)
